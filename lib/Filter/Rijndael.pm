f69d6a8a4c8feb04c91498232c6495bc7a8c50be
Sorin Alexandru Pop <asp@cpan.org>
2014-10-17 20:42:20 +0000

    Import of ASP/Filter-Rijndael-0.03 from CPAN.
    gitpan-cpan-distribution: Filter-Rijndael
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         ASP/Filter-Rijndael-0.03.tar.gz
    gitpan-cpan-author:       ASP
    gitpan-cpan-maturity:     released

--- lib/Filter/Rijndael.pm
+++ lib/Filter/Rijndael.pm
@@ -21,7 +21,7 @@ our @ISA = qw( Exporter DynaLoader );
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = ();
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 bootstrap Filter::Rijndael $VERSION;
 
